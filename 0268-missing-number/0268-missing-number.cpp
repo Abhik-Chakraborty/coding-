f26d634c99dd95411060c5b58cb8be6402b34b79
Abhik C <105542546+Abhik-Chakraborty@users.noreply.github.com>
2024-08-20 23:19:37 +0530

    Time: 7 ms (96.45%), Space: 20.6 MB (60.81%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+    
+    int n = nums.size();
+    
+    int sum = (n * (n + 1)) / 2;
+
+    //Summation of all array elements:
+    int sum2 = 0;
+    for (int i = 0; i < n; i++) {
+        sum2 += nums[i];
+    }
+
+    int missingNum = sum - sum2;
+    return missingNum;
+        
+    }
+};
