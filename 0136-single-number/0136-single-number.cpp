13a5493fd33c4fd7ef5f7a004bba2e47a44e0620
Abhik Chakraborty <105542546+Abhik-Chakraborty@users.noreply.github.com>
2024-09-30 15:36:20 +0530

    Time: 13 ms (64%), Space: 19.5 MB (67.83%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int result = 0;
+        // int result = 0;
 
-        for(int num : nums){
-            result ^= num;
-        }
-        return result;
-        
+        // for(int num : nums){
+        //     result ^= num;
+        // }
+        // return result;
+
+    int n = nums.size();
+
+    // XOR all the elements:
+    int xorr = 0;
+    for (int i = 0; i < n; i++) {
+        xorr = xorr ^ nums[i];
     }
+    return xorr;
+}
+        
+    
 };
