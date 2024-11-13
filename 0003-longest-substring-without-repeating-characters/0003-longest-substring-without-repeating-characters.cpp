faa075c7cc310b452af6645afec85b50009d9f67
Abhik Chakraborty <105542546+Abhik-Chakraborty@users.noreply.github.com>
2024-11-13 13:13:12 +0530

    Time: 0 ms (100%), Space: 11.2 MB (79.85%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,15 +5,15 @@ public:
         int left = 0;
         int maxLength = 0;
 
-    for (int right = 0; right < s.size(); right++) {
-        if (lastSeen[s[right]] != -1) {
+        for (int right = 0; right < s.size(); right++) {
+            if (lastSeen[s[right]] != -1) {
             left = max(left, lastSeen[s[right]] + 1);
-        }
+            }
         
-        lastSeen[s[right]] = right;
-        maxLength = max(maxLength, right - left + 1);
-    }
+            lastSeen[s[right]] = right;
+            maxLength = max(maxLength, right - left + 1);
+        }
 
-    return maxLength;
+        return maxLength;
     }
 };
