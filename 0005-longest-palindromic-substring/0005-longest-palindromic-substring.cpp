33c26e5882332d5265e972ab59e0b9b796baac39
Abhik Chakraborty <105542546+Abhik-Chakraborty@users.noreply.github.com>
2024-11-24 19:58:31 +0530

    Time: 6 ms (90.74%), Space: 8.5 MB (79.78%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -7,31 +7,25 @@ public:
             left--;
             right++;
         }
-        // Return the length of the palindrome
         return right - left - 1;
     }
     
     string longestPalindrome(string s) {
         if (s.empty()) return "";
         
-        int start = 0, end = 0;  // Variables to track the start and end of the longest palindrome
+        int start = 0, end = 0; 
         
         for (int i = 0; i < s.length(); i++) {
-            // 1st case - Palindrome centered at a single character (odd-length palindrome)
             int len1 = expandAroundCenter(s, i, i);
-            // 2nd case -  Palindrome centered between two characters (even-length palindrome)
             int len2 = expandAroundCenter(s, i, i + 1);
             
-            // Get the maximum length of the palindrome found
             int len = max(len1, len2);
             
-            // Update start and end if the new palindrome is longer than the current longest
             if (len > end - start) {
                 start = i - (len - 1) / 2;
                 end = i + len / 2;
             }
         }
-        // Return the longest palindrome substring
         return s.substr(start, end - start + 1);
     }
 };
